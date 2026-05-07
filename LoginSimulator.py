c1783ea67df9e6222f3209b65ded2dc784f53307
AustinMugabe <austinmugabe@outlook.com>
2026-05-07 10:15:18 +0200

    123

--- LoginSimulator.py
+++ LoginSimulator.py
@@ -3,7 +3,7 @@ usernameAndPassword = {}
 def signup():
     username = input("Create a username: ")
     if username in usernameAndPassword:
-        print("Username already exists.")
+        print("Username already exists")
     else:
         password = input("Create a password: ")
         usernameAndPassword[username] = password
@@ -13,13 +13,11 @@ def login():
     username = input("Username: ")
     password = input("Password: ")
     
-    # Check if username exists and password matches
     if username in usernameAndPassword and usernameAndPassword[username] == password:
-        print(f"Login successful! Welcome, {username}.")
+        print(f"Login successful! Welcome, {username}")
     else:
-        print("Invalid username or password.")
+        print("Invalid username or password")
 
-# Main program loop
 while True:
     choice = input("\n1. Register\n2. Login\n3. Exit\nSelect option: ")
     if choice == "1":
